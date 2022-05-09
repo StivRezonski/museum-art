2df6e00bf50f4ba4150e22b0f819e3989e54e91f
Stivrezonski <alexkovach@hotmail.com>
2022-05-09 22:55:32 +0200

    initial commit with setup and mock services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditDetailsComponent } from './detail-preview/edit-details/edit-details.component';
+import { HomeComponent } from './home/home.component';
+import { DetailPreviewComponent } from './detail-preview/detail-preview.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'edit-details', component: EditDetailsComponent },
+  { path: 'detail-preview', component: DetailPreviewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRadioModule } from '@angular/material/radio';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { TreeComponentComponent } from './tree-component/tree-component.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { DetailPreviewComponent } from './detail-preview/detail-preview.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { EditDetailsComponent } from './detail-preview/edit-details/edit-details.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeComponentComponent,
+    DetailPreviewComponent,
+    EditDetailsComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MatRadioModule,
+        MatInputModule,
+        MatIconModule,
+        MatTreeModule,
+        MatButtonModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        FormsModule
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
